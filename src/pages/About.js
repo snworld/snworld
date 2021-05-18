8fdd4ab1a4282511feea2e05ec689724d021de1e
Sam <sn.world00@gmail.com>
2021-05-19 03:33:03 +0600

    minor change

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
       <div className="row">
         <div className="col-md-8">
           <p> 
-            I am a <span>Software Engineer</span> and <span>Information Technology enthusiast</span>. I help business agencies && individuals and create all types of web applications and softwares. I like experiencing new things as well.
+            I am a <span>Software Engineer</span> and <span>Information Technology enthusiast</span>. I help business agencies and individuals by creating all types of web applications and softwares. I like experiencing new things as well.
             <br /><br />
             I'm currently interested in freelance opportunities and remote jobs. So I'm available as a freelancer and you're welcome to send inquiries. You can reach out to me from this address: <a href="mailto:sn.world00@gmail.com"><span className="border-bottom border-info"> sn.world00@gmail.com</span></a>. Also, don't forget to check out my <Link to="/skills"><span className="border-bottom border-info">skills</span></Link> and <Link to="/projects"><span className="border-bottom border-info">projects</span></Link>. Thank you!
           </p>
