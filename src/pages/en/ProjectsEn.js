ffac7c9a98f28bd94927740efad4889fc9e172a5
Sam <sn.world00@gmail.com>
2021-06-09 05:55:22 +0600

    changes

--- src/pages/en/ProjectsEn.js
+++ src/pages/en/ProjectsEn.js
@@ -30,19 +30,19 @@ const ProjectsEn = () => {
           </button>
           <button href="#!" className="category" style={{color: "darkorange"}}>
             <span>Front End</span>
-            <span className="number">6</span>
+            <span className="number">11</span>
           </button>
           <button href="#!" className="category" style={{color: "aqua"}}>
             <span>Back End</span>
-            <span className="number">2</span>
+            <span className="number">7</span>
           </button>
           <button href="#!" className="category" style={{color: "tomato"}}>
             <span>Full Stack</span>
-            <span className="number">2</span>
+            <span className="number">7</span>
           </button>
           <button href="#!" className="category" style={{color: "deepskyblue"}}>
             <span>REST API</span>
-            <span className="number">2</span>
+            <span className="number">3</span>
           </button>
           <button href="#!" className="category" style={{color: "mediumspringgreen"}}>
             <span>WordPress</span>
@@ -58,7 +58,7 @@ const ProjectsEn = () => {
           </button>
           <button href="#!" className="category" style={{color: "magenta"}}>
             <span>Games</span>
-            <span className="number">2</span>
+            <span className="number">1</span>
           </button>
         </div>
         
