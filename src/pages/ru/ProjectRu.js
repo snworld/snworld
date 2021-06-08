b2fa8561762488e52891a036131c3819266ae93e
Sam <sn.world00@gmail.com>
2021-06-08 19:34:49 +0600

    link buttons text change

--- src/pages/ru/ProjectRu.js
+++ src/pages/ru/ProjectRu.js
@@ -12,8 +12,8 @@ const ProjectRu = ({ project }) => {
           <img src={`data:image/png;base64,${project.thumbnail}`} alt="checklist" className="img-fluid m-auto d-block" />
           <div className="ovrly"></div>
           <div className="buttons">
-            <a href={project.demo} rel="noreferrer" target="_blank" className="btn">View Demo</a>
-            <Link to={`/ru/project/${ project._id}`} className="btn">Read More</Link>
+            <a href={project.demo} rel="noreferrer" target="_blank" className="btn">Демо страница</a>
+            <Link to={`/ru/project/${ project._id}`} className="btn">Читать Детали</Link>
           </div>
         </div>
       </div>
