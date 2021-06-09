5afed88f1d5deb3d454b5cc7f87a37ec2c94b7de
Sam <sn.world00@gmail.com>
2021-06-09 06:03:52 +0600

    change

--- src/pages/en/ProjectEn.js
+++ src/pages/en/ProjectEn.js
@@ -12,7 +12,7 @@ const ProjectEn = ({ project }) => {
           <img src={`data:image/png;base64,${project.thumbnail}`} alt="checklist" className="img-fluid m-auto d-block" />
           <div className="ovrly"></div>
           <div className="buttons">
-            <a href={project.demo} rel="noreferrer" target="_blank" className="btn">View Demo</a>
+            <a href={project.demo} rel="noreferrer" target={project.thumbnail === '#' ? '_blank' : '_self'} className="btn">View Demo</a>
             <Link to={`/en/project/${ project._id}`} className="btn">Read More</Link>
           </div>
         </div>
