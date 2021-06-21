885902f897aa4588922c92166e96f7032279cbe3
Sam <sn.world00@gmail.com>
2021-06-22 01:39:47 +0600

    changes

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import NavbarEn from '../NavbarEn'
 
 const Video = () => {
 
           
-  const url = <iframe width="720" height="480" src="https://www.youtube.com/embed/y3f7-2fl-jo" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="myvideo"></iframe>
+  const url = <iframe className="personal-video" src="https://www.youtube.com/embed/y3f7-2fl-jo" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="myvideo"></iframe>
 
   return (
-    <div className="container projects p-4 p-sm-5" style={{maxWidth: "800px"}}>
-      <div className="row m-auto">
-        <div className="col text-center ratio ratio-16x9">
-          { url }
+    <>
+      <NavbarEn />
+      <div className="container projects p-4 p-sm-5" style={{maxWidth: "800px", maxHeight: "500px"}}>
+        <div className="row m-auto">
+          <div className="col ratio ratio-16x9">
+            { url }
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
