6ec789e10f437b46ed4d739abe8f90ecf0a98bac
Samir Nurbek <80161918+snworld@users.noreply.github.com>
2022-07-25 22:22:01 +0600

    Update ProjectPageEn.js

--- src/pages/en/ProjectPageEn.js
+++ src/pages/en/ProjectPageEn.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavbarEn from '../../NavbarEn'
 import LangLinkEn from '../../LangLinkEn'
-import { OpenOutline, ArrowBackCircleOutline } from 'react-ionicons'
+// import { OpenOutline, ArrowBackCircleOutline } from 'react-ionicons'
 import { Link } from 'react-router-dom'
 import { withRouter  } from "react-router-dom";
 
